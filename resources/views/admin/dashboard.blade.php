374a8b5e5ed4cc6e650380ac50fe5db323e213e5
danilopiccinni <danilopiccinni@gmail.com>
2023-05-20 20:30:55 +0200

    feat: style dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,188 @@
 @extends('layouts/admin')
 
 @section('content')
-
-    <div class="container d-flex flex-column align-items-center mt-5">
-        <h6>Dashboard del profilo</h6>
-        <h1>Danilo Piccinni</h1>
-        <h3>Programmatore</h3>
-
+<section class="section-carousel">
+    <div class=" d-flex justify-content-center gap-3 py-5">
+        <div>
+            <div class="carousel carousel-image">
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://fastly.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ" class="d-block w-100" alt="...">
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-rignt d-flex flex-column justify-content-center align-items-center">
+            <div class="carousel">
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="text">
+                            <h3>CREAZIONE SITI WEB PER QUALUNQUE ATTIVITA'</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt fuga a laudantium. Facilis placeat harum quis, temporibus omnis molestiae earum recusandae labore distinctio unde, commodi culpa voluptatibus voluptatum asperiores aut.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat libero similique quisquam nemo commodi et amet necessitatibus, dolores quam magni hic doloribus alias ad, praesentium enim, totam atque iusto consequatur!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="text">
+                            <h3>CONSULENZA E ANALISI TECNICA SITO WEB</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia non cumque minus quas libero culpa facere ab veniam omnis quaerat, illo fuga incidunt possimus accusamus corporis dolorum officiis officia consequatur!
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur possimus ullam, quam dolorem ratione animi dolor autem quisquam ea a molestias voluptatum! Harum ea animi dolor quo ullam sed maiores.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="text">
+                            <h3>CREAZIONE LOGHI ED ELEMENTI GRAFICI PER I SOCIAL</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, hic ut. Officiis beatae tenetur, magni, ducimus sit quam et hic officia harum molestiae, tempora odit aperiam distinctio est. Provident, magnam!
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem perspiciatis sequi deserunt aperiam modi quis dicta, aspernatur, neque laudantium id voluptate voluptatibus blanditiis ducimus minus aliquid ab eos fugiat beatae.
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+</section>
+
+
+<section>
+    <div class="container my-5">
+        <h2>Siti web personalizzati | Un design web unico e una programmazione ad hoc</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci optio possimus, quod, fuga, debitis voluptate eos pariatur id velit volu Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum porro eum facere nihil illo nemo odit eligendi similique quae, totam, doloremque esse magnam neque repudiandae dolore. Exercitationem vero non labore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis architecto distinctio voluptas, ea porro eum dolore asperiores soluta dolorum ratione iusto aperiam ullam atque modi laudantium incidunt aliquam cum? ptatum molestiae perferendis magnam doloremque ullam aut quis vitae alias accusantium.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, quae. Temporibus aliquid commodi, nulla, alias magni nisi ullam amet quo perferendis eaque quos quas! Nobis incidunt ullam minima quibusdam iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facilis assumenda perferendis a aliquam mollitia in dolor consectetur est repudiandae, tempora consequuntur hic, cum nostrum. Quidem, expedita. Consectetur, numquam distinctio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, harum labore quidem quas aperiam tempore, hic quia fugiat obcaecati quos sit a vel amet aliquam praesentium perspiciatis at, beatae nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sequi suscipit earum omnis possimus eligendi voluptatem asperiores, illum voluptatibus laborum harum nulla, magni natus. Saepe dolore adipisci sunt repellendus accusamus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus nemo, corporis temporibus ipsa aspernatur voluptatum, dolore cum maxime saepe quasi quaerat repudiandae veniam pariatur dolorem quae incidunt excepturi asperiores!</p>
+    
+    </div>
+</section>
+
+<section class="section-full-screen">
+
+    <div class="d-flex">
+        <div class="col-4 column-1 column">
+            <img src="https://fastly.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ" class="img-fluid" alt="...">
+        </div>
+        <div class="column-2 col-4 text-end column">
+            <ul>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+            </ul>
+        </div>
+        <div class="column-3 col-4 column">
+            <ul>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+                <li>un po di testo per prendere misure</li>
+            </ul>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="container my-5">
+        <h2>Web design sartoriale | Il proprio sito web, su misura</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci optio possimus, quod, fuga, debitis voluptate eos pariatur id velit volu Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum porro eum facere nihil illo nemo odit eligendi similique quae, totam, doloremque esse magnam neque repudiandae dolore. Exercitationem vero non labore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis architecto distinctio voluptas, ea porro eum dolore asperiores soluta dolorum ratione iusto aperiam ullam atque modi laudantium incidunt aliquam cum? ptatum molestiae perferendis magnam doloremque ullam aut quis vitae alias accusantium.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, harum labore quidem quas aperiam tempore, hic quia fugiat obcaecati quos sit a vel amet aliquam praesentium perspiciatis at, beatae nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sequi suscipit earum omnis possimus eligendi voluptatem asperiores, illum voluptatibus laborum harum nulla, magni natus. Saepe dolore adipisci sunt repellendus accusamus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus nemo, corporis temporibus ipsa aspernatur voluptatum, dolore cum maxime saepe quasi quaerat repudiandae veniam pariatur dolorem quae incidunt excepturi asperiores!</p>
+    </div>
+</section>
+
+
+<section class="banner">
+    <div class=" container-fluid ">
+        <div class="row">
+            <div class="cont-sx col-8">
+                <strong>
+                    100% AL
+                    <br>
+                     CASO TUO
+                </strong>
+            </div>
+            <div class="cont-dx col-4">
+                <strong>Creo per te il sito che fa al caso tuo</strong>
+                <em>Una collaborazione di idee studiate per te insieme a te</em>
+                <span>vuoi saperne di piu?</span>
+            </div>
+        </div>
+        
+    </div>
+
+</section>
+
+<section>
+    <div class="container my-5">
+        <h2>Focus | Siti web custom realizzati in base alla tua attività</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci optio possimus, quod, fuga, debitis voluptate eos pariatur id velit volu Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum porro eum facere nihil illo nemo odit eligendi similique quae, totam, doloremque esse magnam neque repudiandae dolore. Exercitationem vero non labore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facilis architecto distinctio voluptas, ea porro eum dolore asperiores soluta dolorum ratione iusto aperiam ullam atque modi laudantium incidunt aliquam cum? ptatum molestiae perferendis magnam doloremque ullam aut quis vitae alias accusantium.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, harum labore quidem quas aperiam tempore, hic quia fugiat obcaecati quos sit a vel amet aliquam praesentium perspiciatis at, beatae nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sequi suscipit earum omnis possimus eligendi voluptatem asperiores, illum voluptatibus laborum harum nulla, magni natus. Saepe dolore adipisci sunt repellendus accusamus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus nemo, corporis temporibus ipsa aspernatur voluptatum, dolore cum maxime saepe quasi quaerat repudiandae veniam pariatur dolorem quae incidunt excepturi asperiores!</p>
+    </div>
+</section>
+
+
     <a href="{{ route('admin.projects.index') }}">vai alla lista</a>
 @endsection
