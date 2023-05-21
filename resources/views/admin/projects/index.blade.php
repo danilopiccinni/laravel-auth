86ffd282b62709260c18c2febca68aab98daedf1
danilopiccinni <danilopiccinni@gmail.com>
2023-05-21 15:52:23 +0200

    feat: basic style 'admin.index'

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,60 +1,44 @@
 @extends('layouts/admin')
 
 @section('content')
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="{{ route('admin.projects.show', $project) }}">{{ $project->title }}</a>
-                
-            </li>
-        @endforeach
-    </ul>
+<section class="section-admin-index">
 
-    <hr>
-
-    <div>
-        <a href="{{ route('admin.projects.create') }}">Aggiungi un progetto</a>
+    <div class="container my-5">
+        <h1>Portoflio</h1>
+        <h3>Siti web e realizazzioni</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eveniet quaerat id esse ullam, iure dolorum aut ipsum minima placeat harum, at a ea, nobis quis perferendis nostrum omnis illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aspernatur magni blanditiis nisi repellendus esse et similique quibusdam aut odio vitae expedita, tempore sunt, tempora id. Quia ratione dicta modi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos odio quis sequi, hic numquam illo beatae reprehenderit possimus, facere nemo ipsum dolorem laboriosam voluptates doloribus alias eius excepturi culpa.</p>
     </div>
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="{{ route('admin.projects.show', $project) }}">{{ $project->title }}</a>
-                
-            </li>
-        @endforeach
-    </ul>
 
-    <hr>
+    <div class="my-5">
+        <ul>
+            @foreach ($projects as $project)
+                <li>
+                    <a class="cont-card" href="{{ route('admin.projects.show', $project) }}">
+                      <div class="card mb-3" style="width: 70%;">
+                        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4" class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">{{ $project->title }}</h5>
+                              <p class="card-text">{{$project->description}}</p>
+                              <p class="card-text"><small class="text-body-secondary">{{ $project->languages }}</small></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
 
-    <div>
-        <a href="{{ route('admin.projects.create') }}">Aggiungi un progetto</a>
     </div>
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="{{ route('admin.projects.show', $project) }}">{{ $project->title }}</a>
-                
-            </li>
-        @endforeach
-    </ul>
+    
+    
 
-    <hr>
 
-    <div>
-        <a href="{{ route('admin.projects.create') }}">Aggiungi un progetto</a>
-    </div>
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                <a href="{{ route('admin.projects.show', $project) }}">{{ $project->title }}</a>
-                
-            </li>
-        @endforeach
-    </ul>
+</section>
 
-    <hr>
-
-    <div>
-        <a href="{{ route('admin.projects.create') }}">Aggiungi un progetto</a>
-    </div>
 @endsection
