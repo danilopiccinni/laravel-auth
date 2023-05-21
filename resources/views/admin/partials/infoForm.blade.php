2dd86879276f438045989f2003ad5fb80b8da52c
danilopiccinni <danilopiccinni@gmail.com>
2023-05-21 20:16:06 +0200

    modify style infoForm

--- resources/views/admin/partials/infoForm.blade.php
+++ resources/views/admin/partials/infoForm.blade.php
@@ -2,7 +2,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12 col-xl-4 col-lg-5">
+            <div class="cont-sx col-12 col-xl-4 col-lg-5">
                 <strong class="title">Follow me</strong>
                 <div class="title-desc">Nuovi progetti, esperienze lavorative e idee</div>
                 <div class="container-social">
@@ -26,35 +26,28 @@
                     </a>
                 </div>
             </div>
-            <div class="col-12 col-xl-7 col-lg-7 offset-xl-1">
+            <div class="cont-dx col-12 col-xl-7 col-lg-7 offset-xl-1">
                 <strong class="title">Rimani aggiornato</strong>
                 <div class="title-desc">Rimani aggiornato su servizi e prodotti per migliorare il tuo business online</div>
                 <div>
                     <form action="" method=""> 
                         <div class="row"> 	
-                            <div class="col-12 col-sm-6"> 		
-                                <span class="nome">
+                            <div class="cont-input col-12 col-sm-6"> 		
+                                <div class="nome">
                                     <input type="text" name="nome" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="nome">
-                                </span> 	
-                            </div> 	
-                            <div class="col-12 col-sm-6"> 		
-                                <span class="email">
+                                </div> 		
+                                <div class="email">
                                     <input type="email" name="email" value="" size="40" aria-required="true" aria-invalid="false" placeholder="indirizzo email *">
-                                </span> 	
+                                </div> 	
                             </div> 	
                             <div class="col-12"> 		
-                                <span>
-                                    <span>
-                                        <span>
-                                            <label>
-                                                <input type="checkbox" name="newsletter" value="1" aria-invalid="false">
-                                                <span>Desidero ricevere aggiornamenti inerenti ad offerte ed servizi proposti in base a quando espresso nella 
-                                                    <a href="">privacy policy</a>
-                                                </span>
-                                            </label>
-                                        </span>
+                                <label>
+                                    <input type="checkbox" name="newsletter" value="1" aria-invalid="false">
+                                    <span>Desidero ricevere aggiornamenti inerenti ad offerte ed servizi proposti in base a quando espresso nella 
+                                        <a href="">privacy policy</a>
                                     </span>
-                                </span> 	
+                                </label>	
+
                             </div> 	
                             <div class="col-12"> 		
                                 <div class="container-input-submit">
