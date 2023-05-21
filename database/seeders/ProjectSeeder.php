a3301c0e282af6e2a93fd71a0374233ce7212c2e
danilopiccinni <danilopiccinni@gmail.com>
2023-05-21 19:48:25 +0200

    create file config(php) for ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -19,18 +19,34 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i=0; $i < 10; $i++) {
 
+        $arrayProjects = config('projects');
+
+        foreach($arrayProjects as $singleProject) {
             $newProject = new Project();
 
-            $newProject->title = $faker->sentence(2);
-            $newProject->repo = $faker->word();
-            $newProject->description = $faker->text();
-            $newProject->languages = $faker->words(4 , true);
-            $newProject->thumb = $faker->word();
-            $newProject->slug = Str::slug($newProject->title, '-');
+            $newProject->title = $singleProject['title'];
+            $newProject->repo = $singleProject['repo'];
+            $newProject->description = $singleProject['description'];
+            $newProject->languages = $singleProject['languages'];
+            $newProject->thumb = $singleProject['thumb'];
+            $newProject->slug = Str::slug($singleProject['title'], '-');
 
             $newProject->save();
+
+
+        // for($i=0; $i < 10; $i++) {
+
+        //     $newProject = new Project();
+
+        //     $newProject->title = $faker->sentence(2);
+        //     $newProject->repo = $faker->word();
+        //     $newProject->description = $faker->text();
+        //     $newProject->languages = $faker->words(4 , true);
+        //     $newProject->thumb = $faker->word();
+        //     $newProject->slug = Str::slug($newProject->title, '-');
+
+        //     $newProject->save();
         }
     }
 }
