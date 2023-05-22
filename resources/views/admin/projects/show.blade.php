54d7f2a74c9593ac97600b0ce06a921232168c26
danilopiccinni <danilopiccinni@gmail.com>
2023-05-22 18:45:35 +0200

    add modal for DELETE

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -162,13 +162,38 @@
 
         <div class="d-flex justify-content-center gap-5 my-5">
             <a class="btn btn-primary" href="{{route('admin.projects.edit', $project)}}">modifica</a>
+            
+                        <!-- Button trigger modal -->
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Elimina
+              </button>
+              
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      Sicuro di voler eliminare il progetto: {{$project->title}}
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <form action="{{ route('admin.projects.destroy' , $project) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Elimina</button>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
 
 
-            <form action="{{ route('admin.projects.destroy' , $project) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger" type="submit">Elimina</button>
-            </form>
+
 
         </div>
 
